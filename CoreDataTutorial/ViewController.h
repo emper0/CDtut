52b767d440c14f581c4ee55c7c1225eeef733972
dambrowskip <piotr.dambrowski@ue.katowice.pl>
2015-08-22 16:56:18 +0200

    Initial Commit

---
+++ CoreDataTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataTutorial
+//
+//  Created by empero on 22/08/15.
+//  Copyright (c) 2015 InterSID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
