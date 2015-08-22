5b9c09c39ef2fa271d61293015c8a591b8a25934
dambrowskip <piotr.dambrowski@ue.katowice.pl>
2015-08-22 19:07:40 +0200

    crash

--- CoreDataTutorial/AppDelegate.m
+++ CoreDataTutorial/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
 @interface AppDelegate ()
 
 @end
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Fabric with:@[CrashlyticsKit]];
     return YES;
 }
 
--- Podfile
+++ Podfile
@@ -1,2 +1,4 @@
 platform :ios, "7.0"
 pod 'RestKit', '~> 0.25'
+pod 'Fabric'
+pod 'Crashlytics'
