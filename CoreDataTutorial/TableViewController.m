c008a443a50dae8d4c43cc8f033025fd6debe7ce
dambrowskip <piotr.dambrowski@ue.katowice.pl>
2015-08-23 14:20:47 +0200

    Delete

--- CoreDataTutorial/TableViewController.m
+++ CoreDataTutorial/TableViewController.m
@@ -71,6 +71,42 @@
                               sender:self];
 }
 
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if(editingStyle == UITableViewCellEditingStyleDelete){
+        
+        [self.tableView beginUpdates];
+        
+        AppDelegate* appDelegate = [UIApplication sharedApplication].delegate;
+        
+        //delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        
+        //delete userdata from database
+        [_managedObjectContext deleteObject:[_myArray objectAtIndex:indexPath.row]];
+        
+        NSError *error;
+        
+        if(![_managedObjectContext save:&error]){
+            NSLog(@"Cannot save");
+        }
+        
+        //fetch again
+        _myArray = [appDelegate getAllUserRecords];
+        
+        //with endUpdates equal with reload data, so you need to fetch again the array before update the tableview
+        [self.tableView endUpdates];
+        
+    }else if(editingStyle == UITableViewCellEditingStyleInsert){
+        
+    }
+    
+}
+
 
 #pragma mark - Navigation
 
